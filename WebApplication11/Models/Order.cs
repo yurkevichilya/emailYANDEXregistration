009ff40503eccd6b6d706f971ac587facd34a031
yurkevichilya <i.yurkevich.vironit@vironit.ru>
2019-04-11 15:29:01 +0300

    commit

---
+++ WebApplication11/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication11.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual List<ProductOrder> ProductOrder { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Bdate { get; set; }
+    }
+}
---
+++ WebApplication11/Models/ProductOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication11.Models
+{
+    public class ProductOrder
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Id_Product { get; set; }
+
+        public int Id_Order { get; set; }
+    }
+}
